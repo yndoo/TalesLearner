281263f5865128ec6bce960c5518f2e1084bb6be
yndoo <isabelle2k@naver.com>
2025-03-11 02:13:02 +0900

    [ Chore ] 맵 제작 - 카메라 커브 구간 배치

--- Assets/Script/CurveDetect.cs
+++ Assets/Script/CurveDetect.cs
@@ -7,7 +7,7 @@ public class CurveDetect : MonoBehaviour
 {
     public CurveZone zone;
     public Vector3 vec;
-    public bool isCorrectDir; // 마지막 콜리전이 true값
+    public bool isCorrectDir; // 나중에 부딪히는 오브젝트가 true면 +90도, false면 -90도 회전시킴
 
     private void OnTriggerEnter(Collider other)
     {
